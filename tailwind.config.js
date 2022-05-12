6388b328ef4aef2adb3a1af42659d66840cd7271
Kamal Redzwan <kamal.redzwan@gmail.com>
2022-05-12 16:14:49 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  safelist: ["w-64", "w-1/2", "rounded-l-lg", "rounded-r-lg", "bg-gray-200", "grid-cols-4", "grid-cols-7", "h-6", "leading-6", "h-9", "leading-9", "shadow-lg"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  variants: {
+    fill: [],
+    extend: {
+      borderColor: ["focus-visible"],
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
