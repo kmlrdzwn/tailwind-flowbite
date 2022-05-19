17f7d14977f89dacf6316255afcb28153e517f21
Kamal Redzwan <kamal.redzwan@gmail.com>
2022-05-19 16:00:54 +0800

    Added Appear Navbar + Smooth scrolling animation

--- script.js
+++ script.js
@@ -165,3 +165,58 @@ btnYear.addEventListener("click", () => {
 
   duration.forEach((dur) => (dur.innerText = "/year"));
 });
+
+/* ================================= */
+/* ==== Appear NavBar On Scroll ==== */
+/* ================================= */
+
+const sectionHeroEl = document.querySelector("#home");
+const nav = document.querySelector("#navbar");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      navbar.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      navbar.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+/* ================================= */
+/* == Smooth scrolling animation  == */
+/* ================================= */
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
